1bbc35f2dbea04e59ac54918cf396504f2db539c
ludehon <luciendehon@gmail.com>
2020-11-15 15:24:01 +0100

    Fixed test_count

--- tests.py
+++ tests.py
@@ -8,7 +8,7 @@ class Tester:
         content = "these are english words apple aapl  TESLA, micron mu: "
         wf.addWords(content)
         assert(len(wf.count.keys()) == 3)
-        assert(wf.count["apple"] == "1")
-        assert(wf.count["tesla"] == "1")
-        assert(wf.count["micron"] == "1")
+        assert(wf.count["apple"] == 1)
+        assert(wf.count["tesla"] == 1)
+        assert(wf.count["micron"] == 1)
         
